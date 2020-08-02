486ade385f062195155ed6449fadab58cf8105d5
tomito26 <tommybwah@gmail.com>
2020-08-03 01:12:21 +0300

    added a constructed to the javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function Pizza(pizzasize,pizzaCrust,pizzatopping){
+    this.pizzasize = pizzasize
+    this.pizzaCrust = pizzaCrust
+    this.pizzatopping = pizzatopping
+    this.smallPrice = smallPrice
+    
+
+}
 $(document).ready(function(){
     $("form#pizza").submit(function(){
         event.preventDefault();
@@ -10,6 +18,8 @@ $(document).ready(function(){
             pizzaTopping.push($(this).val());
         })
         console.log(pizzaTopping.join(','));
+        
+        // pizza size
         if (pizzasize === 'small'){
           smallPrice = 600
           console.log('The size costs ' + smallPrice)
@@ -28,6 +38,7 @@ $(document).ready(function(){
         else{
             alert ('Ensure you make your full order before submission')
         }
+        //pizza crust
         if (pizzaCrust === 'crispy'){
             crispyPrice = 250
             alert('The crust costs ' + crispyPrice)
@@ -47,8 +58,41 @@ $(document).ready(function(){
             alert('Ensure you make your full order before submission')
 
         }
-           
-         
+        // pizzaTopping
+        let pTopping = pizzaTopping.forEach(function(pizzatopping){
+            alert(pizzatopping)
+        
+            if (pizzatopping === 'pepperoni'){
+                pepperoniPrice = 50;
+                console.log(pepperoni)
+            }
+            else if (pizzatopping === 'mushroom'){
+                mushroomsPrice = 50
+                console.log(mushroomPrice)
+            }
+            else if (pizzatopping === 'sausage'){
+                sausagePrice = 60
+                console.log(sausagePrice)
+            }
+            else if(pizzatopping === 'bacon'){
+                baconPrice = 80
+                console.log(baconPrice)
+            }
+
+            else if(pizzatopping ==='extra-cheese'){
+                extraCheesePrice = 70
+                console.log(extraCheesePrice)
+            }
+            else if (pizzatopping === 'green-pepper'){
+                greenPepperPrice = 50 
+                console.log(greenPepperPrice)
+            }
+            else  {
+                alert ('Ensure you make your full order before submission')
+            }
+        
+
+    });
 
     });
 });
