8ce8fdb0a5b375b12a33f45ed8f8b998835d10bf
tomito26 <tommybwah@gmail.com>
2020-08-02 18:46:06 +0300

    added if and else statement on the size and crust  in js

--- js/script.js
+++ js/script.js
@@ -10,27 +10,41 @@ $(document).ready(function(){
             pizzaTopping.push($(this).val());
         })
         console.log(pizzaTopping.join(','));
-        switch(pizzasize){
-            case'0':
-                price = 0
-                break;
-            case'small':
-               price = 600
-               console.log('The price is ' + price)
-               break;
-            case'medium':
-              price = 800
-              alert('The price is ' + price)
-              break;
-            
-            case 'large':
-                price = 1000
-                alert('The price is ' + price)
-                break;
-
+        if (pizzasize === 'small'){
+          price = 600
+          alert('The size costs ' + price)
 
         }
-       
+        else if (pizzasize === 'medium'){
+            price = 800
+            alert('The size costs ' + price)
+
+        }
+        else if(pizzasize === 'large'){
+            price = 1000
+            alert('The size costs ' + price)
+
+        }
+        else{
+            alert ('Ensure you make a selection inorder to proceed')
+        }
+        if (pizzaCrust === 'crispy'){
+            price = 250
+            alert('The size costs ' + price)
+
+        }
+        else if(pizzaCrust ==='stuffed'){
+            price = 300
+            alert('The size costs ' + price)
+
+        }
+        else if(pizzaCrust === 'glutten-free'){
+            price = 200
+            alert('The size costs ' + price)
+
+        }
+           
+         
 
     });
 });
