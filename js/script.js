22213be325def016744ddd85cc8dc56729a2c937
tomito26 <tommybwah@gmail.com>
2021-02-16 01:33:34 +0300

    changed the javascript function and the  constructor responsible for displaying the data to the user

--- js/script.js
+++ js/script.js
@@ -1,46 +1,54 @@
-// user interface
-$(document).ready(function(){
-    $("form#pizza").submit(function(){
-        event.preventDefault();
-        let pizzasize = parseInt($("#size").val());
-        // alert(pizzasize)
-        let pizzaCrust = parseInt($('#crust').val());
-        // console.log(pizzaCrust)
-        let pizzaTopping = parseInt($("#topping").val());
-        //  alert(pizzaTopping)
-        // alert(totalPrice.add())
-        let quantity = parseInt($("input#quantity").val());
-        // alert(quantity)
-        let delivery = $("#delivery").val()
-        //  alert(delivery)
-    
-        
-    //   business logic
-        function Price(pizzasize,pizzaCrust,pizzaTopping){
-            this.pizzasize = pizzasize
-            this.pizzaCrust = pizzaCrust
-            this.pizzaTopping = pizzaTopping
-        }
-        let totalPrice = new Price(pizzasize,pizzaCrust,pizzaTopping);
-        Price.prototype.add = function(){
-            return this.pizzaTopping + this.pizzasize + this.pizzaCrust
-        }
-        
-        if(quantity > 0 && delivery =='Yes'){
-            prompt("Enter your location for Delivery")
-            alert("Your total price is "  + 'Ksh '+ quantity*(totalPrice.add()) + " and additional delivery cost of Ksh 250")
-            alert("Your Pizza will be delivered shortly")
-        }
-        else if (quantity> 0 && delivery == 'no') {
-            alert("Your total cost is " + 'Ksh '+ quantity*(totalPrice.add()))
-        }
-        
-        else{
-            alert("Ensure you enter the correct submission")
-        }
-        
-        }); 
-    
+// business logic
+class Pizza{
+    constructor(size,crust,topping){
+        this.size = size;
+        this.crust = crust;
+        this.topping = topping
+    }
+   
+}
+class Price{
+    constructor(pizzaSizePrice,pizzaCrustPrice,pizzaToppingPrice){
+        this.pizzaSizePrice = pizzaSizePrice;
+        this.pizzaCrustPrice = pizzaCrustPrice;
+        this.pizzaToppingPrice = pizzaToppingPrice;
+    }
+    get totalPrice(){
+        return this.pizzaSizePrice + this.pizzaCrustPrice + this.pizzaToppingPrice
+    }
+}
+const pizzaSize = ["small","medium","large"]
+const pizzaCrust = ["crispy","stuffed","glutten free"]
+const pizzaTopping = ["peperroni","mushrooms","onions","sausage","bacon","extra-cheese","green-pepper"]
+const sizePice = [600, 800, 1000];
+const crustPrice = [250, 300, 200];
+const toppingPrice = [50, 50, 50, 100, 150, 100, 50];
+let form = document.getElementById("pizza");
 
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const sizePizza = document.getElementById("size").value;
+    const crustPizza = document.getElementById("crust").value;
+    const toppingPizza = document.getElementById("topping").value;
+    console.log(toppingPizza);
 
+    let sizePizzaOrder = pizzaSize[sizePizza - 1];
+    let pizzaCrustOrder = pizzaCrust[crustPizza - 1];
+    let pizzaToppingOrder = pizzaTopping[toppingPizza - 1];
+    console.log(sizePizzaOrder, pizzaCrustOrder, pizzaToppingOrder);
+    let customerOrder = new Pizza(
+        sizePizzaOrder,
+        pizzaCrustOrder,
+        pizzaToppingOrder
+    );
+    console.log(customerOrder);
+
+    const pizzaSizeOrderCost = sizePice[sizePizza -1]
+    const pizzaCrustOrderCost = crustPrice[crustPizza - 1]
+    const pizzaToppingOrderCost = toppingPrice[toppingPizza -1]
+    console.log(pizzaSizeOrderCost,pizzaCrustOrderCost,pizzaToppingOrderCost,)
+
+    let customerOrderCost = new Price(pizzaSizeOrderCost,pizzaCrustOrderCost,pizzaToppingOrderCost)
+    console.log(customerOrderCost,customerOrderCost.totalPrice)
 });
+
